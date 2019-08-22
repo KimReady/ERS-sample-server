07c22f2268138d661f2f2f09cdb1de6b1620b808
JunBi Kim <junbi.kim@navercorp.com>
2019-08-22 15:27:36 +0900

    update admin

--- ers/admin.py
+++ ers/admin.py
@@ -5,7 +5,7 @@ from .models import ErrorLog
 class ReportAdmin(admin.ModelAdmin):
     list_display = ['reg_date', 'log_date', 'package_name', 'app_version', 'sdk_version', 'phone_brand', 'phone_model', 'log_level', 'tag', 'message']
     list_display_links = ['reg_date', 'log_date', 'package_name', 'app_version', 'sdk_version', 'phone_brand', 'phone_model', 'log_level', 'tag', 'message']
-    list_filter = ['sdk_version', 'log_date', 'phone_brand', 'phone_model', 'log_level', 'tag', 'message']
+    list_filter = ['reg_date', 'log_date', 'sdk_version', 'phone_brand', 'phone_model', 'log_level', 'tag', 'message']
     search_fields = ['package_name', 'log_date', 'app_version', 'phone_brand', 'phone_model', 'tag', 'message']
 
 
